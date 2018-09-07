c79ee1a63fba0987cde5251c2470d6c720b6d4da
lipeiheng <liph@wangushengshi.com>
2018-09-07 18:50:37 +0800

    add: 1.新增类CMSApplicationEntrance，项目启动入口

---
+++ src/main/java/org/sao/CMSApplicationEntrance.java
@@ -0,0 +1,17 @@
+package org.sao;
+
+/**
+ * CopyRight @ 深圳市万古盛世互联科技有限公司.Ltd
+ * <br/><b/>
+ * CMSApplicationEntrance，项目入口类
+ *
+ * @author SingleCycle
+ * @date 2018/9/7 ~ 下午 6:48
+ */
+
+public class CMSApplicationEntrance {
+
+	public static void main(String[] args) {
+		System.out.println("现在还没开始写");
+	}
+}
